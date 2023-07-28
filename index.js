01d923e39a607ea5bcb2afa727d1436cb5438eb5
Shaheer Malik <139039230+Malikshaheer222@users.noreply.github.com>
2023-07-28 00:45:50 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+"use strict";
+function admission(uniName) {
+    console.log(`I want to get admission in ${uniName}`);
+}
+admission('air university');
+// Arrow Function
+let sum1 = (num1, num2) => num1 - num2;
+console.log(sum1(167678, 7546));
---
+++ index.ts
@@ -0,0 +1,10 @@
+function admission(uniName:string){
+       console.log(`I want to get admission in ${uniName}`); 
+   
+    }
+     admission('air university')
+
+     // Arrow Function
+
+let sum1=(num1:number,num2:number)=>num1 - num2
+console.log(sum1(167678,7546));
